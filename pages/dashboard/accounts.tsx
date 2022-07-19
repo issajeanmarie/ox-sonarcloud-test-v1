0b1cb22cc0bb1c5ab7231eac1f709262f0884cfa
Issa Jean Marie <issajeanmarie@gmail.com>
2022-07-19 11:07:49 +0300

    Setting up dashboard contents sections

--- pages/dashboard/accounts.tsx
+++ pages/dashboard/accounts.tsx
@@ -3,7 +3,11 @@ import Layout from "../../components/Shared/Layout";
 import WithPrivateRoute from "../../components/Shared/Routes/WithPrivateRoute";
 
 const Accounts = () => {
-  return <Layout />;
+  return (
+    <Layout>
+      <p>Accounts apge</p>
+    </Layout>
+  );
 };
 
 export default WithPrivateRoute(Accounts);
--- pages/dashboard/analytics.tsx
+++ pages/dashboard/analytics.tsx
@@ -3,7 +3,11 @@ import Layout from "../../components/Shared/Layout";
 import WithPrivateRoute from "../../components/Shared/Routes/WithPrivateRoute";
 
 const Analytics = () => {
-  return <Layout />;
+  return (
+    <Layout>
+      <p>Analytics page</p>
+    </Layout>
+  );
 };
 
 export default WithPrivateRoute(Analytics);
