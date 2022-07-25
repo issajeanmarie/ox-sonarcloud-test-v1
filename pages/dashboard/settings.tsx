2dadb2588b86413dc8fa948be5a5bbd92ecd6de1
Elijah97 <elie.gash42@gmail.com>
2022-07-25 19:52:53 +0200

    Settings UI Done

--- pages/dashboard/settings.tsx
+++ pages/dashboard/settings.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import Layout from "../../components/Shared/Layout";
 import WithPrivateRoute from "../../components/Shared/Routes/WithPrivateRoute";
+import TopNavigator from "../../components/Shared/TopNavigator";
+import { Header_Links } from "../../lib/types/links";
+import { Row, Col } from "antd";
+import KPIsTable from "../../components/KPIsTable";
+import Categories from "../../components/Categories";
+import PersonalInfo from "../../components/PersonalInfo";
 
 const Settings = () => {
+  const Links: Header_Links[] = [
+    {
+      label: "PREFERENCES",
+      id: "preferences"
+    },
+    {
+      label: "OX APP",
+      id: "oxapp"
+    }
+  ];
   return (
     <Layout>
-      <p>Settings page</p>
+      {/* PAGES NAVIGATION */}
+      <TopNavigator headerLinks={Links} />
+
+      <div className=" w-[100%] my-5">
+        {/* TOP ROW */}
+        <Row className="p-5 mt-5" gutter={18}>
+          <Col span={14}>
+            {/* KPIs TABLE */}
+            <KPIsTable />
+
+            {/* Categories TABLE */}
+            <Categories />
+          </Col>
+          {/* Personal Info TABLE */}
+          <Col span={10}>
+            <PersonalInfo />
+          </Col>
+        </Row>
+      </div>
     </Layout>
   );
 };
