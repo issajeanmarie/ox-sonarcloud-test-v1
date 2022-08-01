d830ad6c729c91f2a60e4527cb8978d148c90099
patricktunez123 <tunezepatrick@gmail.com>
2022-08-01 09:45:16 +0200

    add analytics menus

--- pages/dashboard/analytics.tsx
+++ pages/dashboard/analytics.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Layout from "../../components/Shared/Layout";
 import WithPrivateRoute from "../../components/Shared/Routes/WithPrivateRoute";
+import TopNavigator from "../../components/Shared/TopNavigator";
+import { Header_Links } from "../../lib/types/links";
 
 const Analytics = () => {
+  const Links: Header_Links[] = [
+    {
+      label: "TRUCKS",
+      id: "trucks"
+    },
+    {
+      label: "REVENUES",
+      id: "revenues"
+    },
+    {
+      label: "MAP",
+      id: "map"
+    },
+    {
+      label: "KPIs",
+      id: "KPIs"
+    }
+  ];
+
   return (
     <Layout>
-      <p>Analytics page</p>
+      <TopNavigator headerLinks={Links} />
     </Layout>
   );
 };
