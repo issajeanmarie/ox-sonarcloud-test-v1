d5e5e613cb4cc3e896c216713b34b3131c056572
patricktunez123 <tunezepatrick@gmail.com>
2022-08-02 14:05:24 +0200

    ft(analytics): analytics - revenue charts

--- components/Analytics/Revenues/AnalyticRevenues.tsx
+++ components/Analytics/Revenues/AnalyticRevenues.tsx
@@ -5,7 +5,11 @@ import CardColWrapper from "../Cards/CardColWrapper";
 import CardRowWrapper from "../Cards/CardRowWrapper";
 import MediumCard from "../Cards/MediumCard";
 import RevenueBreakdownChart from "../Charts/RevenueBreakdownChart";
-import ChartWrapper from "../Wrappers/ChartWrapper";
+import {
+  MediumChartWrapper,
+  SmallChartWrapper
+} from "../Wrappers/ChartWrappers";
+import OrderPerCategoriesChart from "../Charts/OrderPerCategoriesChart";
 
 const AnalyticRevenues = () => {
   return (
@@ -61,9 +65,38 @@ const AnalyticRevenues = () => {
           <span className="opacity-95">Revenue breakdown</span>
         </Col>
         <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-          <ChartWrapper>
+          <MediumChartWrapper>
             <RevenueBreakdownChart />
-          </ChartWrapper>
+          </MediumChartWrapper>
+        </Col>
+      </Row>
+
+      <Row className="pb-10 flex justify-between gap-1">
+        <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8}>
+          <div className="py-5">
+            <span className="opacity-95">Order per Categories</span>
+          </div>
+          <SmallChartWrapper>
+            <OrderPerCategoriesChart />
+          </SmallChartWrapper>
+        </Col>
+
+        <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8}>
+          <div className="py-5">
+            <span className="opacity-95">Weight per Categories</span>
+          </div>
+          <SmallChartWrapper>
+            <OrderPerCategoriesChart />
+          </SmallChartWrapper>
+        </Col>
+
+        <Col xs={24} sm={24} md={7} lg={7} xl={7} xxl={7}>
+          <div className="py-5">
+            <span className="opacity-95">Revenue per Categories</span>
+          </div>
+          <SmallChartWrapper>
+            <OrderPerCategoriesChart />
+          </SmallChartWrapper>
         </Col>
       </Row>
     </>
--- components/Shared/TopNavigator/topNavigator.tsx
+++ components/Shared/TopNavigator/topNavigator.tsx
@@ -1,6 +1,8 @@
 import { FC, useEffect } from "react";
+import CustomInput from "../../Shared/Input";
 import { Header_Links } from "../../../lib/types/links";
-import { Typography } from "antd";
+import { Image, Typography } from "antd";
+import TopNavigatorRightSide from "./TopNavigatorRightSide";
 
 const { Text } = Typography;
 
@@ -22,26 +24,78 @@ const TopNavigator: FC<TopNavigatorProps> = ({
   }, [headerLinks, setActive]);
 
   return (
-    <div className="w-full shadow-[0px_-6px_24px_#0000001A] px-5 bg-ox-white sticky top-0 z-50 flex items-center gap-10">
-      {headerLinks.map((link, index) => {
-        return (
-          <button
-            key={index}
-            className={`py-4 cursor-pointer ${
-              active === link.id && "border-b-4 border-ox-yellow"
-            }`}
-            onClick={() => toggleActiveHandler(link.id)}
-          >
-            <Text
-              className={`${
-                active === link.id ? "font-bold" : "normalText"
-              } animate`}
+    <div className="w-full shadow-[0px_-6px_24px_#0000001A] px-5 bg-ox-white sticky top-0 z-50 flex justify-between items-center">
+      <div className="flex items-center justify-between gap-10">
+        {headerLinks.map((link, index) => {
+          return (
+            <button
+              key={index}
+              className={`py-4 cursor-pointer ${
+                active === link.id && "border-b-4 border-ox-yellow"
+              }`}
+              onClick={() => toggleActiveHandler(link.id)}
             >
-              {link.label}
-            </Text>
-          </button>
-        );
-      })}
+              <Text
+                className={`${
+                  active === link.id ? "font-bold" : "normalText"
+                } animate`}
+              >
+                {link.label}
+              </Text>
+            </button>
+          );
+        })}
+      </div>
+
+      {active === "revenues" && (
+        <TopNavigatorRightSide>
+          <CustomInput
+            type="select"
+            label=""
+            placeholder="Show: Last 7 days"
+            options={[
+              { label: "Item one", value: "one" },
+              { label: "Item two", value: "two" },
+              { label: "Item three", value: "three" }
+            ]}
+            name="sort"
+            suffixIcon={
+              <Image
+                preview={false}
+                src="/icons/expand_more_black_24dp.svg"
+                alt=""
+                width={10}
+              />
+            }
+          />
+          <CustomInput
+            type="date"
+            name="Start"
+            placeholder="Start"
+            suffixIcon={
+              <Image
+                preview={false}
+                src="/icons/ic-actions-calendar.svg"
+                alt=""
+                width={18}
+              />
+            }
+          />
+          <CustomInput
+            type="date"
+            name="End"
+            placeholder="End"
+            suffixIcon={
+              <Image
+                preview={false}
+                src="/icons/ic-actions-calendar.svg"
+                alt=""
+                width={18}
+              />
+            }
+          />
+        </TopNavigatorRightSide>
+      )}
     </div>
   );
 };
