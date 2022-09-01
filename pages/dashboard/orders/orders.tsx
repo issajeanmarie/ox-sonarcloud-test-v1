48067b16be1b3f7d91fa00d193df8bee46355226
Issa Jean Marie <jeanmarieissa@gmail.com>
2022-09-01 09:25:20 +0000

    Logging JWT ENV VARIABLE to see if we get it on dev

--- pages/dashboard/orders/orders.tsx
+++ pages/dashboard/orders/orders.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import Orders from "../../../components/Orders";
 import Layout from "../../../components/Shared/Layout";
 import WithPrivateRoute from "../../../components/Shared/Routes/WithPrivateRoute";
+import { JWT_SECRET } from "../../../config/constants";
 
 const OrdersPage = () => {
+  console.log(
+    "JWT ENV VARIABLE: ",
+    JWT_SECRET || "CAN NOT FIND JWT ENV VARIABLE"
+  );
+
   return (
     <Layout>
       <Orders />
