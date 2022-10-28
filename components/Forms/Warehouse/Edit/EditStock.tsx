ae8c90b8e9fec98c8da5ea92e60d9283a4bdcb2e
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2022-10-28 10:56:06 +0200

    handle more stock

--- components/Forms/Warehouse/Edit/EditStock.tsx
+++ components/Forms/Warehouse/Edit/EditStock.tsx
@@ -60,6 +60,7 @@ const EditStock: FC<EditStockTypes> = ({
 
         <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
           <Input
+            defaultValue={itemToEdit?.supplierName}
             name="supplierId"
             type="select"
             placeholder="Select supplier name"
