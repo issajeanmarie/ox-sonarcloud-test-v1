f5526c87f8207bb91189219c8c6b5aba0c66e0a0
Elie Gashagaza <elie.gash42@gmail.com>
2022-11-24 12:42:54 +0200

    OX App Release integration

--- lib/types/settings.ts
+++ lib/types/settings.ts
@@ -93,3 +93,12 @@ export type GetRepairService = {
 export type MakeCategoryParentRequest = {
   id: number;
 };
+export type OxAppReleaseResponse = {
+  id: number;
+  version: string;
+  apkUrl: string;
+  phone: string;
+  releaseDateTime: string;
+  size: boolean;
+  changeLog: string;
+};
