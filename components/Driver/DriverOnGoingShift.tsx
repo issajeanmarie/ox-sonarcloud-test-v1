38cd557af2a0608916f77865d52e9f61ea28e570
Issa Jean Marie <jeanmarieissa@gmail.com>
2023-01-24 16:44:33 +0200

    adding: lock icon on locked orders

--- components/Driver/DriverOnGoingShift.tsx
+++ components/Driver/DriverOnGoingShift.tsx
@@ -4,8 +4,27 @@ import Row from "antd/lib/row";
 import CustomButton from "../Shared/Button";
 import Divider from "antd/lib/divider";
 import TruckOverviewCard from "../Analytics/Trucks/TruckOverviewCard";
+import { FC } from "react";
+import { DriverProfileResponse } from "../../lib/types/Accounts/drivers";
+import TimeOverviewCard from "../Analytics/Trucks/TimeOverviewCard";
+import { useEndShiftMutation } from "../../lib/api/endpoints/Accounts/driversEndpoints";
+import { handleAPIRequests } from "../../utils/handleAPIRequests";
 
-const DriverOnGoingShift = () => {
+interface Props {
+  driverData: DriverProfileResponse;
+}
+
+const DriverOnGoingShift: FC<Props> = ({ driverData }) => {
+  const [endShift, { isLoading }] = useEndShiftMutation();
+
+  const handleEndShift = () => {
+    handleAPIRequests({
+      request: endShift,
+      id: driverData?.payload?.profileInfo?.id,
+      showSuccess: true
+    });
+  };
+
   return (
     <Row className="bg-[#FFFFFF] rounded shadow-[0px_0px_19px_#00000008] mt-4">
       <Row justify="space-between" align="middle" className="w-full p-8">
@@ -16,7 +35,12 @@ const DriverOnGoingShift = () => {
         </Col>
 
         <Col flex="none">
-          <CustomButton type="danger" size="small">
+          <CustomButton
+            type="danger"
+            size="small"
+            loading={isLoading}
+            onClick={handleEndShift}
+          >
             <span className="red">STOP SHIFT</span>
           </CustomButton>
         </Col>
@@ -34,7 +58,12 @@ const DriverOnGoingShift = () => {
             xxl={{ span: 12 }}
             className="mb-8"
           >
-            <TruckOverviewCard data={{ name: "Orders", num: 12323 }} />
+            <TimeOverviewCard
+              data={{
+                name: "Duration",
+                profileInfo: driverData?.payload?.profileInfo
+              }}
+            />
           </Col>
 
           <Col
@@ -45,7 +74,12 @@ const DriverOnGoingShift = () => {
             xxl={{ span: 12 }}
             className="mb-8"
           >
-            <TruckOverviewCard data={{ name: "Duration", num: 12323 }} />
+            <TruckOverviewCard
+              data={{
+                name: "Orders",
+                num: driverData?.payload?.ongoingOrders
+              }}
+            />
           </Col>
         </Row>
       </div>
