c30ae441e43c8983a0fb784b0f75baf6bb6ad842
Issa Jean Marie <jeanmarieissa@gmail.com>
2023-02-16 23:54:28 +0200

    creating: privacy policy page

--- pages/dashboard/policy/index.tsx
+++ pages/dashboard/policy/index.tsx
@@ -1,79 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Image from "antd/lib/image";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import { useRouter } from "next/router";
-import Dropdown from "antd/lib/dropdown";
 import Button from "../../../components/Shared/Button";
-import {
-  END_USER_AGREEMENTS_CONTENT,
-  PRIVACY_POLICY_CONTENT
-} from "../../../config/constants";
+import { PRIVACY_POLICY_CONTENT } from "../../../config/constants";
 import { routes } from "../../../config/route-config";
 
-const POLICY_MENUS = [
-  {
-    id: 1,
-    name: "Policy",
-    url: routes.Policy.url,
-    page: "POLICY"
-  },
-
-  {
-    id: 2,
-    name: "Agreements",
-    url: routes.Agreements.url,
-    page: "AGREEMENTS"
-  }
-];
-
-interface MenuTypes {
-  page: string;
-  url: string;
-  id: number;
-  name: string;
-}
-
 const Policy = () => {
-  const [activeMenu, setActiveMenu] = useState("");
   const router = useRouter();
-  const { page } = router.query;
-
-  const handleMenuChange = (menu: MenuTypes) => {
-    router.push(menu.url);
-  };
-
-  useEffect(() => {
-    setActiveMenu(String(page || "POLICY"));
-  }, [page]);
-
-  const CONTENT =
-    activeMenu === "POLICY"
-      ? PRIVACY_POLICY_CONTENT
-      : END_USER_AGREEMENTS_CONTENT;
-
-  const file_link =
-    activeMenu === "POLICY"
-      ? "/docs/OX_Privacy_Policy.pdf"
-      : "/docs/OX_End_User_License_Agreement.pdf";
-
-  const dropdownMenu = (
-    <div className="max-h-[50vh]  overflow-y-auto radius4 p-6 pb-2 bg-white rounded shadow-[0px_0px_19px_#2A354808] border">
-      {POLICY_MENUS?.map((menu) => (
-        <Row
-          key={menu.id}
-          onClick={() => handleMenuChange(menu)}
-          className={`${
-            activeMenu === menu.page && "font-bold"
-          } mb-4 pointer hover:text-ox-dark text-gray-600 hover:font-medium`}
-        >
-          <Col>{menu.name}</Col>
-        </Row>
-      ))}
-    </div>
-  );
 
   return (
     <div className="relative leading-[2]">
@@ -90,38 +27,16 @@ const Policy = () => {
         </Col>
 
         <Col>
-          <Row align="middle" gutter={24}>
-            <Col>
-              <Dropdown
-                overlay={dropdownMenu}
-                trigger={["hover"]}
-                placement="bottomRight"
-              >
-                <div className="pointer flex gap-2 items-center bg-gray-100 hover:bg-gray-200 rounded p-2 px-4">
-                  <span className="font-medium">Menu</span>{" "}
-                  <Image
-                    preview={false}
-                    src="/icons/expand_more_black_24dp.svg"
-                    alt=""
-                    width={8}
-                  />
-                </div>
-              </Dropdown>
-            </Col>
-
-            <Col>
-              <a href={file_link} download>
-                <Button
-                  form="AddNewDriver"
-                  loading={false}
-                  type="secondary"
-                  htmlType="submit"
-                >
-                  Download PDF
-                </Button>
-              </a>
-            </Col>
-          </Row>
+          <a href="/docs/ox_privacy_policy.pdf" download>
+            <Button
+              form="AddNewDriver"
+              loading={false}
+              type="secondary"
+              htmlType="submit"
+            >
+              Download PDF
+            </Button>
+          </a>
         </Col>
       </Row>
 
@@ -132,18 +47,16 @@ const Policy = () => {
           className="bg-white shadow-md rounded m-auto p-8 sm:p-8 md:p-12 lg:p-14 xl:p-16 2xl:p-16 bg-white w-[95%] sm:w-[95%] md:w-[90%] lg:w-[70%] xl:w-[60%] 2xl:w-[50%] h-[fit-content] pb-32"
           style={{ zIndex: 100 }}
         >
-          <span className="text-[32px] font-bold block leading-[1.2] mb-3">
-            {CONTENT.title}
-          </span>
+          <span className="text-[32px] font-bold block">OX Privacy Policy</span>
 
           <p className="text-[16px] italic text-gray-500">
-            Last updated on:{" "}
-            <span className="text-ox-dark">{CONTENT.lastUpdate}</span>
+            Last updated on{" "}
+            <span className="text-ox-dark">22nd January 2023</span>
           </p>
 
-          <p className="mt-12 mb-6">{CONTENT?.intro}</p>
+          <p className="mt-12 mb-6">{PRIVACY_POLICY_CONTENT?.intro}</p>
 
-          {CONTENT?.sections?.map((section, index) => (
+          {PRIVACY_POLICY_CONTENT?.sections?.map((section, index) => (
             <div key={section.id}>
               <h1 className="font-bold text-black text-[18px] mt-12">
                 {index + 1}. {section.title}
